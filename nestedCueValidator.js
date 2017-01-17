bb7ad87c3761ad5066d0bab72c39486e8facd888
Jesse Hall <jessehall3@gmail.com>
2017-01-16 21:36:24 -0500

    all files

---
+++ nestedCueValidator.js
@@ -0,0 +1,154 @@
+var Cue = function(index, startTime, endTime, text, children){
+    this.index = index;
+    this.startTime = startTime;
+    this.endTime = endTime;
+    this.text = text || "";
+    this.children = children || [];
+}
+
+var validate_and_return_array_of_nested_cues = function(cueList){
+    
+    if( cueList.length < 1 ){
+        console.log("The cue list is empty.");
+        return -1;
+    }
+    
+    var isValidNest = function( childCue, rhsCueItem ){
+        // rhsCueItem will always be the end-time of a parent cue
+        if( rhsCueItem === undefined){
+            // there are no open parent cues
+            return true;
+        }
+        return childCue.endTime <= rhsCueItem.time
+    };
+    
+    var CueItem = function(cue, type){
+        this.index = cue.index;
+        this.stampType = type;
+        this.time = cue[type];
+    }
+    
+    // left-hand stack
+    var lhs = [];
+    // right-hand stack
+    var rhs = [];
+    // final list of cues (with nest child cues) to return
+    var nestedCues = [];
+    
+    var previousCue = null;
+    var currentCue = null;
+    var position = 0;
+    
+    while( position < cueList.length){
+        currentCue = cueList[position]
+        if( currentCue.startTime >= currentCue.endTime){
+            console.log(" ERROR: For cue ID# " + currentCue.index + ", start-time must come before end-time");
+            return -1;
+        }
+
+        if( position == 0 ){
+            lhs.push( new CueItem(currentCue, 'startTime' ) );
+            rhs.push( new CueItem(currentCue, 'endTime' ) );
+            
+            nestedCues.push(currentCue);
+            
+            position++;
+            continue;
+        }
+        
+        // previousCueItem is now the last cueItem in the LHS (left-hand stack)
+        var previousCueItem = lhs[lhs.length-1]
+        var previousCueIndex = previousCueItem.index;
+
+        // If previousCue is a start-time
+        // Case #1
+        if( currentCue.startTime < previousCueItem.time && previousCueItem.stampType == "startTime"){
+            console.log(" ERROR: The start-time of cue ID# " + currentCue.index + 
+                        " cannot come before its previous cue, ID# " + previousCueIndex + ".");
+            return -1;
+            
+        }
+        // Case #2
+        if( currentCue.startTime == previousCueItem.time && previousCueItem.stampType == "startTime"){
+            if( isValidNest( currentCue, rhs[rhs.length-1] ) ){
+                if( rhs.length ){
+                 parentIndex = rhs[rhs.length-1].index;
+                 parentCue = cueList[parentIndex];
+                 parentCue.children.push(currentCue);
+                }
+                else{
+                    nestedCues.push(currentCue);
+                }
+                
+                lhs.push( new CueItem(currentCue, 'startTime' ) );
+                rhs.push( new CueItem(currentCue, 'endTime' ) );
+                
+                position++;
+                continue;
+            }
+            
+            console.log(" ERROR: The end-time of cue ID# " + currentCue.index + 
+                        " extends beyond its parent cue, ID# " + previousCueIndex + ".");
+            return -1;
+        }
+        // Case #3
+        if( currentCue.startTime > previousCueItem.time && previousCueItem.stampType == "startTime"){
+            if( isValidNest( currentCue, rhs[rhs.length-1] ) ){
+                if( rhs.length ){
+                 parentIndex = rhs[rhs.length-1].index;
+                 parentCue = cueList[parentIndex];
+                 parentCue.children.push(currentCue);
+                }
+                else{
+                    nestedCues.push(currentCue);
+                }
+                
+                lhs.push( new CueItem(currentCue, 'startTime' ) );
+                rhs.push( new CueItem(currentCue, 'endTime' ) );
+                
+                position++;
+                continue;
+            }
+            else {
+                // close this parent cue
+                lhs.push( rhs.pop() );
+            }
+            continue;
+        }
+
+        // If previousCue is an end-time
+        // Case #4
+        if( currentCue.startTime < previousCueItem.time && previousCueItem.stampType == "endTime"){
+            console.log(" ERROR: The start-time of cue ID# " + currentCue.index + 
+                        " cannot come after its previous cue, ID# " + previousCueIndex + ".");
+            return -1;
+            
+        }
+        // Case #5 (and #6)
+        if( currentCue.startTime >= previousCueItem.time && previousCueItem.stampType == "endTime"){
+            if( isValidNest( currentCue, rhs[rhs.length-1] ) ){
+                if( rhs.length ){
+                 parentIndex = rhs[rhs.length-1].index;
+                 parentCue = cueList[parentIndex];
+                 parentCue.children.push(currentCue);
+                }
+                else{
+                    nestedCues.push(currentCue);
+                }
+                
+                lhs.push( new CueItem(currentCue, 'startTime' ) );
+                rhs.push( new CueItem(currentCue, 'endTime' ) );
+                
+                position++;
+                continue;
+            }
+            else {
+                // close this parent cue
+                lhs.push( rhs.pop() );
+            }
+            continue;
+        }
+    }
+
+    return nestedCues
+};
---
+++ nestedCuesToHtml.js
@@ -0,0 +1,26 @@
+var nestedCuesToHtml = function(nestedCues){
+    var $mainArray = $('<ul></ul>');
+    $mainArray.attr('id', 'mainArray');
+    
+    var traverse = function(arr, $ul){
+        for(var i=0; i < arr.length; i++){
+            var cue = arr[i];
+            var $newLi = $('<li></li>')
+            $newLi.attr('id', 'cueId-' + cue.index )
+            $newLi.text( cue.text );
+            $ul.append($newLi)
+            
+            if( cue.children.length ){
+                $newUl = $('<ul></ul>');
+                $newUl.attr('class', 'children');
+                $newLi.append($newUl);
+                
+                traverse(cue.children, $newUl)
+            }
+        }
+    }
+    
+    traverse(nestedCues, $mainArray);
+    
+    return $mainArray;
+}
